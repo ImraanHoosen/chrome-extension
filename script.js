2af3849bce59fc2d7ce6304d35df455eafae7222
ImraanHoosen <142743532+ImraanHoosen@users.noreply.github.com>
2024-12-12 05:54:56 +0200

    .

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
+//Declare an empty array to store the saved leads
+let myLeads = [];
+
+//Get references to the input field, button and unordered list
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 
+//Add an event listener to the button that will add the input value to the array when clicked
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
-  console.log(myLeads);
+
+  renderLeads(); //Render the list of leads on the page
 });
 
-for (let i = 0; i < myLeads.length; i++) {
-  ulEl.textContent += myLeads[i] + " ";
+//Create a string of list items from the array and add it to the unordered list
+function renderLeads() {
+  let listItems = "";
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems += "<li>" + myLeads[i] + "</li>";
+  }
+  ulEl.innerHTML = listItems;
 }
